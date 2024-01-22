1baf2fd103d358707fd1b0ecdd91d1f4b3d5f507
Ben Packer <ben.paul.ryan.packer@gmail.com>
2024-01-21 19:29:11 -0500

    feat: implement everything

---
+++ README.md
@@ -0,0 +1,160 @@
+# graphile-saga
+
+A TypeScript library for managing complex, multi-step processes with automatic rollback capabilities in the event of a failure. Built on top of `graphile-worker` and `zod`, `graphile-saga` provides a robust solution for implementing the saga pattern with strong type safety and easy-to-follow cancellation semantics.
+
+## Installation
+
+To install `graphile-saga`, you can use `npm`, `yarn`, or `bun`:
+
+```bash
+bun add graphile-saga
+```
+
+## Usage
+
+To create a saga, first define the initial payload schema with `zod`, then use `createSaga` to define the saga steps. Each step can have a `run` function and an optional `cancel` function for rollback purposes.
+
+```typescript
+// makeSeservationSaga.ts
+import { createSaga } from 'graphile-saga';
+import { z } from 'zod';
+import { hotelService, flightService, carService } from './travelServices';
+
+// Define your saga
+const makeReservationSaga = createSaga('makeReservation', z.object({
+  hotelId: z.number(),
+  airlineId: z.number(),
+  carId: z.number(),
+}))
+  .addStep({
+    name: 'reserveHotel',
+    run: async (initialPayload, priorResults, helpers) => {
+      const hotelConfirmation = await hotelService.reserve(initialPayload.hotelId);
+      return { hotelConfirmation };
+    },
+    cancel: async (initialPayload, priorResults, runResult, helpers) => {
+      await hotelService.cancelReservation(runResult.hotelConfirmation);
+    },
+  })
+  .addStep({
+    name: 'reserveFlight',
+    run: async (initialPayload, priorResults, helpers) => {
+      const flightConfirmation = await flightService.bookFlight(initialPayload.airlineId);
+      return { flightConfirmation };
+    },
+    cancel: async (initialPayload, priorResults, runResult, helpers) => {
+      await flightService.cancelFlight(runResult.flightConfirmation);
+    },
+  })
+  .addStep({
+    name: 'reserveCar',
+    run: async (initialPayload, priorResults, helpers) => {
+      const carAvailable = await carService.checkAvailability(initialPayload.carId);
+      if (!carAvailable) {
+				// Cancel throws a custom error which triggers rollbacks all the way up
+        helpers.cancel('Car not available');
+      }
+      const carConfirmation = await carService.reserveCar(initialPayload.carId);
+      return { carConfirmation };
+    },
+    cancel: async (initialPayload, priorResults, runResult, helpers) => {
+      await carService.cancelCarReservation(runResult.carConfirmation);
+    },
+  });
+```
+
+Not every step needs to have a `cancel` function. If a step does not have a `cancel` function, the saga will automatically progress
+rolling back to the previous step.
+
+```typescript
+// worker.ts
+import { makeReservationSaga } from './makeReservationSaga';
+
+const taskList = {
+	// this has all of your regular graphile worker tasks
+	// just add your saga task lists to it
+	...makeReservationSaga.getTaskList()
+	...otherSaga.taskList()
+} as const;
+```
+
+Then you can queue a `makeReservation` job by any allowed method, and the whole saga will run.
+
+## Danger
+
+We have no way of preventing you from queueing a saga task directly, since it's just a regular graphile worker task.
+However, if you do this, the job will fail, because we intercept and add extra properties to the payload 
+to make the continuation and rollback semantics work correctly.
+
+To make this easier to avoid (and to cut down on some Typescript noise) we provide an `AddJobFn` type that excludes the
+ability to queue a saga job directly.
+
+```typescript
+import { createTask, createTaskList } from 'graphile-worker-zod';
+import { run, TaskList } from 'graphile-worker';
+import { AddJobFn } from 'graphile-saga';
+import { makeReservationSaga } from './makeReservationSaga';
+import { z } from 'zod'
+
+const sendEmail = createTask(
+  z.object({
+    email: z.string().email(),
+  }),
+  async (payload, jobHelpers) => {
+    // payload is typed as { email: string }
+    // send email
+  }
+)
+
+const taskList = {
+  sendEmail,
+	...makeReservationSaga.getTaskList()
+} as const; // you need this as const declaration!!!
+
+let runner: Runner;
+
+export const startWorker = async () => {
+  runner = await run({
+    pgPool: pool // definition not shown,
+    taskList: taskList as TaskList // cast required because type has extra info graphile-worker doesn't want
+  })
+}
+
+/**
+ * If graphile-worker-zod's AddJobFn was used, the typeof addJob would be:
+ * (taskName: "sendEmail", payload: { email: string }) => Promise<Job>
+ * (taskName: "makeReservation", payload: { hotelId: number; airlineId: number; carId: number; }) => Promise<Job>
+ * (taskName: "makeReservation|reserveHotel", payload: { hotelId: number; airlineId: number; carId: number; }) => Promise<Job>
+ * (taskName: "makeReservation|reserveHotel|cancel", ...
+ * (taskName: "makeReservation|reserveFlight, ...
+ * ...
+ * 
+ * It's too much! With 'graphile-saga's AddJobFn you get just the first two.
+ * 
+ */
+export const addJob: AddJobFn<typeof TaskList> = async (taskName, payload) => {
+  if (!runner) {
+    throw new Error('Add job called before worker started');
+  }
+
+  return runner.addJob(taskName, payload);
+}
+```
+
+## Motivation
+
+The saga pattern is a sequence of transactions that updates multiple services in a distributed system. If one transaction fails, the saga automatically executes compensating transactions to undo the changes made by the preceding successful transactions. `graphile-saga` leverages TypeScript's type system to ensure that each step of the saga receives the correct types for payloads and results, reducing the likelihood of runtime errors and making the code easier to reason about.
+
+## Under The Hood
+
+`graphile-saga` maps each step in a saga to specific task names that are used by `graphile-worker`. For a saga named `mySaga` with steps `step1`, `step2`, etc., the task names would be:
+
+- `mySaga`: The initial task that starts the saga.
+- `mySaga:step1`: The task corresponding to the first step of the saga.
+- `mySaga:step1:cancel`: The task that handles cancellation of the first step, if defined.
+- `mySaga:step2`: The task for the second step, and so on.
+
+Each task is strongly typed, and the task list is automatically generated based on the saga definition, ensuring that all steps adhere to the defined types and logic.
+
+`graphile-saga` also automates the process of queueing the next step in the saga, as well as any necessary cancellation steps. This ensures that you don't have to manually manage the flow of the saga or the rollback logic.
+
---
+++ src/saga.test.ts
@@ -0,0 +1,278 @@
+import { describe, test, expect, mock, beforeAll } from "bun:test";
+import { z } from "zod";
+import { createSaga } from "./saga";
+import { runTaskListOnce, quickAddJob, TaskList, run } from "graphile-worker";
+import { Pool } from "pg";
+
+const pgPool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+describe("graphile-saga", () => {
+  beforeAll(async () => {
+    // This forces migraitons to run
+    const runner = await run(
+      { pgPool },
+      {
+        nothing: async () => {},
+      }
+    );
+    await runner.stop();
+    await pgPool.query("truncate graphile_worker._private_jobs;");
+  });
+
+  test("saga.getTaskList should return a task list with the initial task, all the steps, and the cancel steps", async () => {
+    const sagaName = "test-get-task-list";
+
+    const saga = createSaga(sagaName, z.object({ name: z.string() }))
+      .addStep({
+        name: "step1",
+        run: async () => {},
+        cancel: async (payload) => {},
+      })
+      .addStep({
+        name: "step2",
+        run: async () => {},
+        cancel: async () => {},
+      });
+
+    const taskList = saga.getTaskList();
+
+    expect(taskList).toHaveProperty(`${sagaName}`);
+    expect(taskList).toHaveProperty(`${sagaName}|step1`);
+    expect(taskList).toHaveProperty(`${sagaName}|step1|cancel`);
+    expect(taskList).toHaveProperty(`${sagaName}|step2`);
+    expect(taskList).toHaveProperty(`${sagaName}|step2|cancel`);
+  });
+
+  test("running the saga should run the first function", async () => {
+    const sagaName = "test-run-saga";
+
+    const fn = mock(async () => {});
+
+    const saga = createSaga(sagaName, z.object({ name: z.string() })).addStep({
+      name: "step1",
+      run: fn,
+    });
+
+    const taskList = saga.getTaskList() as TaskList;
+
+    await quickAddJob({ pgPool }, sagaName, { name: "test" });
+
+    const client = await pgPool.connect();
+    await runTaskListOnce({ pgPool }, taskList, client);
+    await client.release();
+
+    expect(fn).toHaveBeenCalled();
+  });
+
+  test("running the first function should queue the second function with prior results", async () => {
+    const sagaName = "test-run-saga-two";
+
+    const fn2 = mock(async (shouldBeResult1: string) => {});
+
+    const saga = createSaga(sagaName, z.object({ name: z.string() }))
+      .addStep({
+        name: "step1",
+        run: async (initialPayload, priorResults, helpers) => {
+          return "result1";
+        },
+      })
+      .addStep({
+        name: "step2",
+        run: async (payload, previousResults) => {
+          return fn2(previousResults.step1);
+        },
+      });
+
+    const taskList = saga.getTaskList() as TaskList;
+
+    await quickAddJob({ pgPool }, sagaName, { name: "test" });
+
+    const client = await pgPool.connect();
+    // Need to run once for each step
+    await runTaskListOnce({ pgPool }, taskList, client);
+    await runTaskListOnce({ pgPool }, taskList, client);
+    // And release
+    await client.release();
+
+    expect(fn2).toHaveBeenCalledWith("result1");
+  });
+
+  test("calling helpers.cancel should run the cancel function for the step before", async () => {
+    const sagaName = "test-cancel-saga";
+
+    let cancelPayload: string | undefined = undefined;
+    let cancelPreviousResults: Record<string, any> | undefined = undefined;
+    let cancelRunResult: string | undefined = undefined;
+
+    const fn1Cancel = mock(
+      async (initialPayload, previousResults, runResult) => {
+        cancelPayload = initialPayload;
+        cancelPreviousResults = previousResults;
+        cancelRunResult = runResult;
+        return true;
+      }
+    );
+
+    const saga = createSaga(sagaName, z.string())
+      .addStep({
+        name: "step1",
+        run: async (initialPayload, priorResults, helpers) => {
+          return "result1";
+        },
+        cancel: fn1Cancel,
+      })
+      .addStep({
+        name: "step2",
+        run: async (payload, previousResults, helpers) => {
+          return helpers.cancel("No reason");
+        },
+      });
+
+    const taskList = saga.getTaskList() as TaskList;
+
+    await quickAddJob({ pgPool }, sagaName, "hello");
+
+    const client = await pgPool.connect();
+    // First step
+    await runTaskListOnce({ pgPool }, taskList, client);
+    // Second step which throws cancel
+    await runTaskListOnce({ pgPool }, taskList, client);
+    // Third step which is the first steps cancel
+    await runTaskListOnce({ pgPool }, taskList, client);
+    // And release
+    await client.release();
+
+    expect(fn1Cancel).toHaveBeenCalled();
+
+    expect(cancelPayload as any).toEqual("hello");
+    expect(cancelPreviousResults as any).toEqual({ step1: "result1" });
+    expect(cancelRunResult as any).toEqual("result1");
+  });
+
+  test("calling helpers.cancel should cascade up the cancel functions, skipping steps if a cancel step is missing", async () => {
+    const sagaName = "test-cancel-saga-cascade";
+
+    const fn1Cancel = mock(
+      async (initialPayload, previousResults, runResult) => {
+        return true;
+      }
+    );
+
+    const fn2Cancel = mock(
+      async (initialPayload, previousResults, runResult) => {
+        return true;
+      }
+    );
+
+    const saga = createSaga(sagaName, z.string())
+      .addStep({
+        name: "step1",
+        run: async (initialPayload, priorResults, helpers) => {
+          return "result1";
+        },
+        cancel: fn1Cancel,
+      })
+      .addStep({
+        name: "step2",
+        run: async (payload, previousResults, helpers) => {
+          return "result2";
+        },
+        cancel: fn2Cancel,
+      })
+      .addStep({
+        name: "step3",
+        run: async (payload, previousResults, helpers) => {
+          return helpers.cancel("No reason");
+        },
+      });
+
+    const taskList = saga.getTaskList() as TaskList;
+
+    await quickAddJob({ pgPool }, sagaName, "hello");
+
+    const client = await pgPool.connect();
+    // First step
+    await runTaskListOnce({ pgPool }, taskList, client);
+    // Second step
+    await runTaskListOnce({ pgPool }, taskList, client);
+    // Third step which cancels
+    await runTaskListOnce({ pgPool }, taskList, client);
+    // Fourth step which is the second steps cancel
+    await runTaskListOnce({ pgPool }, taskList, client);
+    // Fifth step which is the first steps cancel
+    await runTaskListOnce({ pgPool }, taskList, client);
+    // And release
+    await client.release();
+
+    expect(fn2Cancel).toHaveBeenCalled();
+    expect(fn1Cancel).toHaveBeenCalled();
+  });
+
+  test("calling helpers.cancel should cascade up the cancel functions, skipping steps if a cancel step is missing", async () => {
+    const sagaName = "test-cancel-saga-cascade";
+
+    const fn1Cancel = mock(
+      async (initialPayload, previousResults, runResult) => {
+        return true;
+      }
+    );
+
+    const fn3Cancel = mock(
+      async (initialPayload, previousResults, runResult) => {
+        return true;
+      }
+    );
+
+    const saga = createSaga(sagaName, z.string())
+      .addStep({
+        name: "step1",
+        run: async (initialPayload, priorResults, helpers) => {
+          return "result1";
+        },
+        cancel: fn1Cancel,
+      })
+      .addStep({
+        // Step 2 has no cancel function
+        name: "step2",
+        run: async (payload, previousResults, helpers) => {
+          return "result2";
+        },
+      })
+      .addStep({
+        name: "step3",
+        run: async (payload, previousResults, helpers) => {
+          return "result3";
+        },
+        cancel: fn3Cancel,
+      })
+      .addStep({
+        name: "step4",
+        run: async (payload, previousResults, helpers) => {
+          return helpers.cancel("No reason");
+        },
+      });
+
+    const taskList = saga.getTaskList() as TaskList;
+
+    await quickAddJob({ pgPool }, sagaName, "hello");
+
+    const client = await pgPool.connect();
+    // First step
+    await runTaskListOnce({ pgPool }, taskList, client);
+    // Second step
+    await runTaskListOnce({ pgPool }, taskList, client);
+    // Third step
+    await runTaskListOnce({ pgPool }, taskList, client);
+    // Fourth step which cancels
+    await runTaskListOnce({ pgPool }, taskList, client);
+    // Fifth step which is the third steps cancel
+    await runTaskListOnce({ pgPool }, taskList, client);
+    // Sixth step which is the first steps cancel
+    await runTaskListOnce({ pgPool }, taskList, client);
+    // And release
+    await client.release();
+
+    expect(fn3Cancel).toHaveBeenCalled();
+    expect(fn1Cancel).toHaveBeenCalled();
+  });
+});
